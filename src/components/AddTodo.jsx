d27a114e6636af2f35a4077525a1438f9cbcb9a5
Dinesh45kumer <dineshepd001@gmail.com>
2026-06-03 16:36:07 +0530

    first commit

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function AddTodo({ onAdd }) {
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await axios.post('http://localhost:8000/api/todos/', {
+            title,
+            completed: false
+        });
+        // const response = await axios.post('https://todo-backend-yi3f.onrender.com/api/todos/', {
+        //     title,
+        //     completed: false
+        // });
+        onAdd(response.data);
+        setTitle('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Add todo"
+                required
+            />
+            <button type="submit">Add</button>
+        </form>
+    );
+}
+
+export default AddTodo;
